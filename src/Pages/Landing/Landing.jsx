31b31d1db39102bbcbe83a6b58d97ae455cb5794
YitagesTheDeveloper <yitagesadane88@gmail.com>
2025-12-05 10:17:00 -0900

    Update Landing.jsx

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Carousel from '../../components/Carousel/Carousel'
 import Category from '../../components/Category/Category'
-import Product from '../../components/product/Product'
+import Product from '../../components/Product/Product'
 import Layout from '../../components/Layout/Layout'
 
 const Landing = () => {
